83e276a34a451a76bcb2c3d16060507b26bcc09b
houdiini <houdiini@gmail.com>
2017-08-13 17:57:35 +0300

    add script task

---
+++ gulp/script.js
@@ -0,0 +1,30 @@
+let gulp       = require("gulp"),
+    uglify     = require("gulp-uglify"),
+    // babelify   = require("babelify"),
+    browserify = require("gulp-browserify"),
+    vueify     = require("vueify"),
+    rename     = require("gulp-rename"),
+    {config}     = require("../package.json");
+
+gulp.task('script', () => {
+    let transformations = [];
+    if (config.es6)
+        transformations.push("babelify");
+    if (config.vue)
+        transformations.push('vueify')
+
+    return gulp.src(config.src.js + '/*.js')
+            .pipe(browserify({
+                transform: transformations
+            }))
+            .pipe(gulp.dest(config.build.js))
+            // .pipe(uglify())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(gulp.dest(config.build.js))
+});
+
+gulp.task('script:watch', ['script'], () => {
+    gulp.watch(config.src.js + '/*.js', ['script']);
+})
